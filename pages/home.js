8d0761b992d883a86b13338e6af4c105f93c93f1
Jvsm123 <Jvsm123@outlook.com>
2022-04-13 12:57:24 -0300

    add hotjar and google analyticcs

--- pages/home.js
+++ pages/home.js
@@ -1,14 +1,16 @@
 import Head from '../components/Header';
+import { useRouter } from 'next/router';
 
 export default function Home()
 {
+	const router = useRouter();
 	return(
 		<>
 			<Head/>
 
 			<p>Aqui é um P</p>
 
-			<button type="button" onClick={() => router.push('/')}>
+			<button type="button" onClick={() => router.back()}>
 				Raiz
 			</button>
 		</>
