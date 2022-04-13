0fa1c4c9a6a277752629e72627d5ba1a8744190b
Jvsm123 <Jvsm123@outlook.com>
2022-04-12 22:30:31 -0300

    add hotjar and google analyticcs

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 import '../styles/globals.css'
 
+import { hotjar } from 'react-hotjar';
+import { useEffect } from 'react';
+
+const HJID = process.env.ENV_HJID || "Mock";
+const HJSV = process.env.ENV_HJSV || "Mock";
+const GID = process.env.ENV_GTAGID || "Mock";
+
 export default function MyApp({ Component, pageProps })
 {
-	return <Component {...pageProps} />
+	useEffect( () =>
+	{
+		if( HJID === "Mock" || HJSV === "Mock" ) return;
+
+		hotjar.initialize( HJID, HJSV );
+	}, []);
+
+	return <Component {...pageProps, { gid: GID } }/>
 }
